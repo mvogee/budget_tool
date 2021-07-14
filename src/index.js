cbdd1ebdf8d8870c39c9033ee354f88a09c9c41c
Matthew Vogee <mvogee@yahoo.com>
2021-07-13 18:59:26 -0700

    add and delete budget line items from budgets page

--- src/index.js
+++ src/index.js
@@ -38,9 +38,23 @@ app.get("/", (req, res) => {
     res.render("home");
 });
 
+// * this is my currently how i delete category items from the user facing page.
+app.post("/deleteBudgetItm", (req, res) => {
+    console.log("I'm going to delete something");
+        console.log(req.body.deleteCategory);
+        let sql = "DELETE FROM budgets WHERE id=?";
+        sqlconnection.query(sql, req.body.deleteCategory, (err, result) => {
+            if (err) {
+                return console.log(err);
+            }
+            console.log(result);
+        });
+        res.redirect("/budgets");
+});
 
 app.route("/budgets")
     .get((req, res) => {
+        console.log("get");
         let sql = "SELECT * FROM budgets";
         sqlconnection.query(sql, (err, result) => {
             if (err) {
@@ -76,15 +90,16 @@ app.route("/budgets")
         });
     })
     .delete((req, res) => {
+        console.log("I'm going to delete something");
         console.log(req.body.categoryId);
         let sql = "DELETE FROM budgets WHERE id=?";
-        sqlconnection.query(sql, req.body.categoryId, (err, result) => {
+        sqlconnection.query(sql, req.body.deleteCategory, (err, result) => {
             if (err) {
                 return console.log(err);
             }
             console.log(result);
         });
-        res.redirect("/budgets"); // change to /budgets once route is handled
+        res.redirect("/budgets");
     });
 
 app.listen(port, () => {
