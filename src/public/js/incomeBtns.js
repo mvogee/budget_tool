7e5924077f5abc3a461fe4123f697243fc952b35
Matthew Vogee <mvogee@yahoo.com>
2021-08-06 15:49:59 -0700

    projected Income update form. Route not handled yet

---
+++ src/public/js/incomeBtns.js
@@ -0,0 +1,42 @@
+
+
+let editBtns = document.querySelectorAll(".editButton");
+editBtns.forEach(btn => {
+    btn.addEventListener("click", (event) => {
+        console.log("edit button was clicked");
+        edtiIncomeBtnClick(event);
+    });
+});
+
+function edtiIncomeBtnClick(event) {
+    let itmId = event.target.getAttribute("itmId");
+    let name = event.target.getAttribute("name");
+    let hourlyRate = event.target.getAttribute("hourlyRate");
+    let taxRate = event.target.getAttribute("taxRate");
+    let tithe = event.target.getAttribute("tithe");
+    let retirement = event.target.getAttribute("retirement");
+
+    let idField = document.querySelector(".incomeItemUpdate .idInput");
+    let nameField = document.querySelector(".incomeItemUpdate .nameInput");
+    let hourlyRateField = document.querySelector(".incomeItemUpdate .hourlyRateInput");
+    let taxRateField = document.querySelector(".incomeItemUpdate .taxRateInput");
+    let titheFeild = document.querySelector(".incomeItemUpdate .titheInput");
+    let retirementField = document.querySelector(".incomeItemUpdate .retirementInput");
+
+    idField.setAttribute("value", itmId);
+    nameField.setAttribute("value", name);
+    hourlyRateField.setAttribute("value", hourlyRate);
+    taxRateField.setAttribute("value", taxRate);
+    titheFeild.setAttribute("value", tithe);
+    retirementField.setAttribute("value", retirement);
+
+    document.querySelector("#popupIncomeItemUpdate").classList.toggle("hidePopupEdit");
+};
+
+
+let cancelBtn = document.querySelector(".incomeItemUpdate .cancelBtn");
+cancelBtn.addEventListener("click", (event) => {
+    console.log("cancel was clicked");
+    document.querySelector("#popupIncomeItemUpdate").classList.toggle("hidePopupEdit");
+    document.querySelector("#popupIncomeItemUpdate .incomeItemUpdate").reset();
+});
--- src/public/js/thisMonthBtns.js
+++ src/public/js/thisMonthBtns.js
@@ -1,9 +1,9 @@
 
 let editBtnSpending = document.querySelectorAll(".editBtnSpend");
 editBtnSpending.forEach(btn => {
-    btn.addEventListener("click", (EventSource) => {
+    btn.addEventListener("click", (event) => {
         console.log("edit button was pressed");
-        editButtonSpendigClick(EventSource);
+        editButtonSpendigClick(event);
     });
 });
 
@@ -30,30 +30,34 @@ function editButtonSpendigClick(event) {
     document.querySelector("#popupEditSpending").classList.toggle("hidePopupEdit");
 };
 
-let editButtonIncome = document.querySelectorAll(".editBtnIncome");
-editButtonIncome.forEach(btn => {
+let editButtonDeposit = document.querySelectorAll(".editBtnDeposit");
+editButtonDeposit.forEach(btn => {
     btn.addEventListener("click", (event) => {
         console.log("btn clicked");
-        let itmId = event.target.getAttribute("itmId");
-        let name = event.target.getAttribute("name");
-        let incomeAmount = event.target.getAttribute("incomeAmount");
-        let ddate = event.target.getAttribute("depositDate");
-
-        let nameField =  document.querySelector("#updateDepositItem .editNameInput");
-        let amountField = document.querySelector("#updateDepositItem .editAmountInput");
-        let dateField = document.querySelector("#updateDepositItem .editDateInput");
-        let idField = document.querySelector("#updateDepositItem .itmId");
-
-        nameField.setAttribute("Value", name);
-        amountField.setAttribute("Value", incomeAmount);
-        dateField.setAttribute("Value", ddate);
-        idField.setAttribute("Value", itmId);
-        //! Should toggle the popup menu to display block
-        //! Should see if hidePopupEdit is already in classList or not and toggle based on that
-        document.querySelector("#popupEditDeposit").classList.toggle("hidePopupEdit");
+        editButtonDepositClick(event);
     });
 });
 
+
+function editButtonDepositClick(event) {
+    let itmId = event.target.getAttribute("itmId");
+    let name = event.target.getAttribute("name");
+    let incomeAmount = event.target.getAttribute("incomeAmount");
+    let ddate = event.target.getAttribute("depositDate");
+
+    let nameField =  document.querySelector("#updateDepositItem .editNameInput");
+    let amountField = document.querySelector("#updateDepositItem .editAmountInput");
+    let dateField = document.querySelector("#updateDepositItem .editDateInput");
+    let idField = document.querySelector("#updateDepositItem .itmId");
+
+    nameField.setAttribute("Value", name);
+    amountField.setAttribute("Value", incomeAmount);
+    dateField.setAttribute("Value", ddate);
+    idField.setAttribute("Value", itmId);
+    //! Should toggle the popup menu to display block
+    //! Should see if hidePopupEdit is already in classList or not and toggle based on that
+    document.querySelector("#popupEditDeposit").classList.toggle("hidePopupEdit");
+}
 
 // Cancle button functionality
 let cancelEditBtnSpending = document.querySelector("#editCancelBtnSpending");
@@ -63,8 +67,8 @@ cancelEditBtnSpending.addEventListener("click", (type) => {
     document.querySelector("#updateSpendingItem").reset();
 });
 
-let cancelEditBtnIncome = document.querySelector("#editCancelBtnDeposit");
-cancelEditBtnIncome.addEventListener("click", (type) => {
+let cancelEditBtnDeposit = document.querySelector("#editCancelBtnDeposit");
+cancelEditBtnDeposit.addEventListener("click", (type) => {
     console.log("cancel was pressed");
     document.querySelector("#popupEditDeposit").classList.toggle("hidePopupEdit");
     document.querySelector("#updateDepositItem").reset();
