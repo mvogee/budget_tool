2475b538db0f4a89780c3cad0819f7c4f8211eb5
Matthew Vogee <mvogee@yahoo.com>
2021-08-31 18:01:03 -0700

    compiled data needed for month to monht graph

--- src/public/js/overview.js
+++ src/public/js/overview.js
@@ -1,5 +1,5 @@
 //! route is not set up for this yet
-async function getYearData(route) {
+async function getData(route) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
     let opts = {
@@ -12,10 +12,7 @@ async function getYearData(route) {
     return (data);
 };
 
-
-async function doStuff() {
-    let purchases = await getYearData("/getYearPurchases");
-    let incomes = await getYearData("/getYearIncomes");
+function setYearTotal(purchases, incomes) {
     let totalPurchases = 0;
     let totalIncomes = 0;
     purchases.forEach(element => {
@@ -26,9 +23,54 @@ async function doStuff() {
     });
     const yearsTotal = (totalIncomes - totalPurchases).toFixed(2);
     document.querySelector(".yearSavings .data").innerHTML = yearsTotal;
-    console.log(purchases);
-    console.log(incomes);
-    
+};
+
+
+async function mapPurchases(purchases) {
+    let purchaseMap = new Map();
+    purchases.forEach(element => {
+        let date = new Date(element.purchaseDate).getMonth() + 1;
+        if (purchaseMap.has(date)) {
+            purchaseMap.set(date, purchaseMap.get(date) + element.amount);
+        }
+        else {
+            purchaseMap.set(date, element.amount);
+        }
+    });
+    return (purchaseMap);
+};
+
+async function mapIncomes(incomes) {
+    let incomeMap = new Map();
+
+    incomes.forEach(element => {
+        let date = new Date(element.depositDate).getMonth() + 1;
+        if (incomeMap.has(date)) {
+            incomeMap.set(date, incomeMap.get(date) + element.amount);
+        }
+        else {
+            incomeMap.set(date, element.amount);
+        }
+    });
+    return (incomeMap);
+};
+
+async function monthToMonthGraph(purchases, incomes) {
+    let purchaseMap = await mapPurchases(purchases);
+    let incomeMap = await mapIncomes(incomes);
+    let monthToMonth = new Map();
+
+    for (let i = 1; i <= new Date().getMonth() + 1; i++) {
+        monthToMonth.set(i, ((incomeMap.has(i) ? incomeMap.get(i) : 0) - (purchaseMap.has(i) ? purchaseMap.get(i) : 0)));
+    }
+    console.log(monthToMonth);
 }
 
-doStuff();
+async function overView() {
+    let purchases = await getData("/getYearPurchases");
+    let incomes = await getData("/getYearIncomes");
+    setYearTotal(purchases, incomes);
+    monthToMonthGraph(purchases, incomes);
+}
+
+overView();
