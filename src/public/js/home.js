8b0e34ae44e53aa53b751e805c1e0c7e7ff8a8d9
Matthew Vogee <mvogee@yahoo.com>
2021-11-06 15:44:10 -0700

    updated nav for pages that user needs to log in. updated home page styles

---
+++ src/public/js/home.js
@@ -0,0 +1,6 @@
+
+document.querySelector("#overview-li").style.display = "none";
+document.querySelector("#budget-li").style.display = "none";
+document.querySelector("#income-li").style.display = "none";
+document.querySelector("#thismonth-li").style.display = "none";
+document.querySelector("#logout-button").style.display = "none";
--- src/public/js/login.js
+++ src/public/js/login.js
@@ -44,9 +44,3 @@ function validateNewAcc(event) {
 
 
 // * hide navigation items with no access and log out button
-
-document.querySelector("#overview-li").style.display = "none";
-document.querySelector("#budget-li").style.display = "none";
-document.querySelector("#income-li").style.display = "none";
-document.querySelector("#thismonth-li").style.display = "none";
-document.querySelector("#logout-button").style.display = "none";
